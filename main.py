67e8a19661cb8de0098f4dc0ecdae654c95e96b0
Yuki <risaton@risaton.net>
2023-01-10 15:45:31 +0900

    commit

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ bot = discord.Bot()
 #
 @bot.slash_command()
 async def play(ctx: ApplicationContext):
-    vc = await ctx.user.voice.channel.connect()
-    vc.play(discord.FFmpegPCMAudio("VR - Virtual Reality (Music Only) _ KMNZ [Official Music Video]-lYQGWVSVl_Y.mp3"))
+    #discord.opus.load_opus("/opt/homebrew/lib/libopus.dylib")
+    vc:VoiceClient = await ctx.user.voice.channel.connect()
+    source = discord.FFmpegPCMAudio("VR - Virtual Reality (Music Only) _ KMNZ [Official Music Video]-lYQGWVSVl_Y.mp3")
+    source2 = PCMVolumeTransformer(source, 0.1)
+    vc.play(source2)
 
 
 
-bot.run("MTAyMTY3NDYxODY4MTAzMjcyNA.GSkizf.emLroGkSY4GEGFePy6AXMsO5_g7P8q3sQp-r0s")
+bot.run("MTAyMTY3NDYxODY4MTAzMjcyNA.Gx2Kc4.EAEFziBXq-rHVFQkZlJ8bXbGJ120Abvod-3aj8")
