ddeb03f9332debc1380e85e2e9b665ee596e3582
Yuki <risaton@risaton.net>
2023-01-12 02:51:25 +0900

    commit

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ import wavelink
 
 bot = discord.Bot()
 
+
 async def connect_nodes():
     await bot.wait_until_ready()
     node = await wavelink.NodePool.create_node(
         bot=bot,
-        host="bot-5.nekocat.info",
+        host="192.168.11.151",
         port=2333,
         password='youshallnotpass'
-        #password=""
+        # password=""
     )
     # node.set_volume = 0.1
 
@@ -21,16 +22,15 @@ async def connect_nodes():
 @bot.slash_command()
 async def play(ctx, url: str):
     vc = ctx.voice_client
-    # if not vc:  # check if the bot is not in a voice channel
-    vc:wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)  # connect to the voice channel
-    await vc.set_volume(2)
+    if not vc:  # check if the bot is not in a voice channel
+        vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)  # connect to the voice channel
+        await vc.set_volume(2)
     if ctx.author.voice.channel.id != vc.channel.id:  # check if the bot is not in the voice channel
         return await ctx.respond("BOTと同じボイスチャンネルにいる必要があります！")  # return an error message
     # if "soundcloud" in url:
     # else:
     song = await wavelink.YouTubeTrack.search(query=url, return_first=True)
     # song = await wavelink.SoundCloudTrack.search(query=url, return_first=True)
-
 
     if not song:  # check if the song is not found
         return await ctx.respond("該当なし.")  # return an error message
@@ -42,6 +42,8 @@ async def play(ctx, url: str):
 
     await vc.play(song)  # play the songe
     await ctx.respond(f"再生中: `{vc.source.title}`")  # return a message
+
+
 #
 # @bot.slash_command()
 # async def play(ctx: ApplicationContext):
@@ -56,13 +58,16 @@ async def stop(ctx: ApplicationContext):
     vc = ctx.voice_client
     await vc.disconnect()
     await ctx.respond(embed=Embed(description="Disconnecting..."))
+
+
 @bot.event
 async def on_ready():
-  await connect_nodes() # connect to the server
+    await connect_nodes()  # connect to the server
+
 
 @bot.event
 async def on_wavelink_node_ready(node: wavelink.Node):
-  print(f"{node.identifier} is ready.") # print a message
+    print(f"{node.identifier} is ready.")  # print a message
 
 
-bot.run("ODg5NzUxNDYyODM2NTY4MDk2.G-flGO.gNzt9eMbBx4Y48ckTcAYJhkJEQpf8kc6lkNGOo")
+bot.run("ODg5NzUxNDYyODM2NTY4MDk2.G8SIOH.VBTtTsocvcfGf1jGwPDk9nALUC1w0gToXbUOls")
