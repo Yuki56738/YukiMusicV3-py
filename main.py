dcc4c92a9c89ab4ba138443a0f1c7249f07836a5
Yuki <risaton@risaton.net>
2023-09-19 17:34:53 +0900

    commit

--- main.py
+++ main.py
@@ -1,79 +1,28 @@
 import os
 import discord
+import lavaplay
 from discord import *
+from discord import Intents
 from dotenv import load_dotenv
+
 load_dotenv()
 TOKEN = os.environ.get("DISCORD_TOKEN")
-intents = discord.Intents.all()
+intents: Intents = discord.Intents.all()
 bot = discord.Bot(intents=intents)
 
-"""
-import wavelink
-async def connect_nodes():
-    await bot.wait_until_ready()
-    node = await wavelink.NodePool.create_node(
-        bot=bot,
-        host="risaton.net",
-        port=2333,
-        password="yukilava",
-        https=False
-    )
+
+
+
+lava = lavaplay.Lavalink()
+
+lavalink = lava.create_node(
+    host="risaton.net",
+    port=2333,
+    password="yukilava",
+    user_id=123
+)
+
 @bot.event
-async def on_voice_update(member: Member, before: VoiceState, after: VoiceState):
-    if len(before.channel.members) == 1:
-        await before.channel.guild.voice_client.disconnect()
-song_queue = {}
-async def on_song_end(guild_id: int, vc: wavelink.Player):
-    song_queue[guild_id].pop(0)
-    if song_queue[guild_id]:
-        await vc.play(song_queue[guild_id][0])
-@bot.slash_command()
-async def play(ctx: ApplicationContext, url: str):
-    vc = ctx.voice_client
-    await ctx.defer()
-    if not vc:
-        vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
-        await vc.set_volume(2)
-    if ctx.author.voice.channel.id != vc.channel.id:
-        return await ctx.followup.send("BOTと同じボイスチャンネルにいる必要があります！")
-    # if len(vc.channel.members) == 0:
-    #     await vc.disconnect()
-    song = await wavelink.YouTubeTrack.search(query=url, return_first=True)
-    if not song:
-        return await ctx.followup.send("該当なし.")
-    if ctx.guild_id not in song_queue:
-        song_queue[ctx.guild_id] = {}
-    song_queue[ctx.guild_id].append(song)
-    if len(song_queue[ctx.guild_id]) == 1:
-        await vc.play(song_queue[ctx.guild_id][0])
-        await ctx.followup.send(f"再生中: `{vc.source.title}`")
-    else:
-        await ctx.followup.send(f"`{song.title}` をキューに追加しました。")
-    while vc.is_playing():
-        await asyncio.sleep(1)
-    await on_song_end(ctx.guild_id, vc)
-@bot.slash_command()
-async def skip(ctx: ApplicationContext):
-    await ctx.defer()
-    vc = ctx.voice_client
-    if not vc or not vc.is_playing():
-        return await ctx.followup.send("現在再生中の曲がありません。")
-    vc.stop()
-    if len(song_queue[ctx.guild_id]) > 0:  # if there are other songs in the queue, play the next one
-        song = song_queue.pop(0)
-        await vc.play(song)
-        await ctx.followup.send(f"再生中: `{vc.source.title}`")
-    else:  # if the queue is empty, disconnect from the voice channel
-        await vc.disconnect()
-        await ctx.followup.send("曲が終了しました。")
-@bot.slash_command()
-async def stop(ctx: ApplicationContext):
-    vc = ctx.voice_client
-    await vc.disconnect()
-    await ctx.respond(embed=Embed(description="Disconnecting..."))
-    song_queue[ctx.guild_id] = []
-@bot.event
-"""
 async def on_ready():
     # await connect_nodes()  # connect to the server
     print(f"Logged in as: {bot.user.name}")
@@ -81,7 +30,17 @@ async def on_ready():
     for x in bot.guilds:
         print(x)
     print("--------------------------")
-bot.load_extension("musiccog")
+    print("Connecting to node...")
+    global lavalink
+    lavalink.user_id = bot.user.id
+    lavalink.set_event_loop(bot.loop)
+    lavalink.connect()
+
+
+@lavalink.listen(lavaplay.ReadyEvent)
+async def on_lava_ready(event: lavaplay.ReadyEvent):
+    print("Connected to node.")
+# bot.load_extension("musiccog")
 
 
 # @bot.event
